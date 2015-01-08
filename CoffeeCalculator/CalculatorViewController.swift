5845999fae541e979a56e8446af5759cfd4c906d
Tim Davies <mail@timdavi.es>
2015-01-08 16:20:11 +0000

    Remove unneeded code

--- CoffeeCalculator/CalculatorViewController.swift
+++ CoffeeCalculator/CalculatorViewController.swift
@@ -95,15 +95,4 @@ class CalculatorViewController: UIViewController {
             waterDecrementButton.enabled = true
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
